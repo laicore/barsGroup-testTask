a091a8c6a5b58ea4a3c5f27a6b4444cbf4934d62
LaiCORE <laicore@users.noreply.github.com>
2020-03-19 00:38:04 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 mysql -uroot -proot -e "create database testBarsdb"
 mysql -uroot -proot -e "create user 'boxBars'@'localhost' IDENTIFIED WITH mysql_native_password BY 'boxBars'"
-mysql -uroot -proot -e "grant all on testBarsdb.* to 'boxBars'@'localhost'
+mysql -uroot -proot -e "grant all on testBarsdb.* to 'boxBars'@'localhost'"
 python3 ~/barsGroup-testTask/siths/manage.py migrate
